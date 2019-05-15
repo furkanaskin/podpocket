69d33c8bd553fb4978cdc742278a5ceeb7d31108
Tolga Taştimur <tastimurtolga@gmail.com>
2019-05-15 09:21:35 +0300

    proile tabı hazırlanıyor

--- app/src/main/java/com/furkanaskin/app/podpocket/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/furkanaskin/app/podpocket/ui/profile/ProfileFragment.kt
@@ -1,8 +1,12 @@
 package com.furkanaskin.app.podpocket.ui.profile
 
+import android.os.Bundle
+import android.view.View
 import com.furkanaskin.app.podpocket.R
 import com.furkanaskin.app.podpocket.core.BaseFragment
 import com.furkanaskin.app.podpocket.databinding.FragmentProfileBinding
+import com.furkanaskin.app.podpocket.db.entities.UserEntity
+import org.jetbrains.anko.doAsync
 
 /**
  * Created by Furkan on 16.04.2019
@@ -15,6 +19,30 @@ class ProfileFragment : BaseFragment<ProfileViewModel, FragmentProfileBinding>(P
 
     override fun getLayoutRes(): Int = R.layout.fragment_profile
 
-    override fun init() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (user == null) {
+            //TODO üye girişine yönlendirecek.
+            return
+        }
+
+        mBinding.viewModel?.userData = user
+        editProfile()
     }
+
+    fun editProfile() {
+        mBinding.buttonSave.setOnClickListener {
+            val user = UserEntity(
+                    uniqueId = mAuth.currentUser?.uid ?: "",
+                    name = mBinding.editTextNameSurname.text?.toString(),
+                    surname = mBinding.editTextNameSurname.text?.toString(),
+                    birthday = mBinding.etBirthday.text?.toString(),
+                    email = mBinding.editTextEmail.text?.toString())
+
+            viewModel.changeUserData(user)
+        }
+
+    }
+
 }
