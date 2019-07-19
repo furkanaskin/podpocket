4915c7f39e5587856628f17eb0a446d35b0aeb00
faskN <seeingisknow@gmail.com>
2019-07-19 23:49:36 +0300

    Tab usage fix.

--- app/src/main/java/com/furkanaskin/app/podpocket/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/furkanaskin/app/podpocket/ui/splash/SplashActivity.kt
@@ -19,82 +19,82 @@ import io.reactivex.rxkotlin.addTo
 
 class SplashActivity : BaseActivity<SplashActivityViewModel, ActivitySplashBinding>(SplashActivityViewModel::class.java) {
 
-	private val composite = CompositeDisposable()
-	private var listenerFlag: Boolean? = true
+    private val composite = CompositeDisposable()
+    private var listenerFlag: Boolean? = true
 
-	override fun getLayoutRes() = R.layout.activity_splash
+    override fun getLayoutRes() = R.layout.activity_splash
 
-	override fun initViewModel(viewModel: SplashActivityViewModel) {
-		binding.viewModel = viewModel
-	}
+    override fun initViewModel(viewModel: SplashActivityViewModel) {
+        binding.viewModel = viewModel
+    }
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-		// ---------------------------------------------------------------------------------------
-		// If you don't know what is going over there please don't replace this block or xml file.
-		// ---------------------------------------------------------------------------------------
+        // ---------------------------------------------------------------------------------------
+        // If you don't know what is going over there please don't replace this block or xml file.
+        // ---------------------------------------------------------------------------------------
 
-		binding.imageViewAppLogo.viewTreeObserver.addOnGlobalLayoutListener {
+        binding.imageViewAppLogo.viewTreeObserver.addOnGlobalLayoutListener {
 
-			if (listenerFlag != false) {
+            if (listenerFlag != false) {
 
-				listenerFlag = false
-				val textRectf = Rect()
-				val logoRectf = Rect()
+                listenerFlag = false
+                val textRectf = Rect()
+                val logoRectf = Rect()
 
-				binding.textViewAppName.getGlobalVisibleRect(textRectf)
-				val textX = textRectf.centerX()
-				val textY = textRectf.centerY()
+                binding.textViewAppName.getGlobalVisibleRect(textRectf)
+                val textX = textRectf.centerX()
+                val textY = textRectf.centerY()
 
-				binding.imageViewAppLogo.getGlobalVisibleRect(logoRectf)
-				val logoX = logoRectf.centerX()
-				val logoY = logoRectf.centerY()
+                binding.imageViewAppLogo.getGlobalVisibleRect(logoRectf)
+                val logoX = logoRectf.centerX()
+                val logoY = logoRectf.centerY()
 
 
-				val moveY = textY - logoY
-				val moveX = textX - logoX - textRectf.width() / 2 - (Constants.MainAnimationConstants.LOGO_WIDTH / 2).dpToPx() - Constants.MainAnimationConstants.MARGIN_BETWEEN_LOGO_AND_APPNAME.dpToPx()
+                val moveY = textY - logoY
+                val moveX = textX - logoX - textRectf.width() / 2 - (Constants.MainAnimationConstants.LOGO_WIDTH / 2).dpToPx() - Constants.MainAnimationConstants.MARGIN_BETWEEN_LOGO_AND_APPNAME.dpToPx()
 
-				RxAnimation.together(
-						binding.textViewAppName.fadeOut(Constants.MainAnimationConstants.NO_DURATION))
-						.subscribe().addTo(composite)
+                RxAnimation.together(
+                        binding.textViewAppName.fadeOut(Constants.MainAnimationConstants.NO_DURATION))
+                        .subscribe().addTo(composite)
 
-				RxAnimation.sequentially(
-						binding.imageViewAppLogo.fadeIn(Constants.MainAnimationConstants.LONG_DURATION),
-						binding.imageViewAppLogo.resize(300, 300),
-						binding.imageViewAppLogo.rotation(7200f, Constants.MainAnimationConstants.EXTRA_LONG_DURATION),
-						RxAnimation.together(
-								binding.imageViewAppLogo.resize(Constants.MainAnimationConstants.LOGO_WIDTH_INT, Constants.MainAnimationConstants.LOGO_HEIGHT_INT),
-								binding.imageViewAppLogo.translation(pixelsToDps(this, moveX.toInt()).toFloat(), pixelsToDps(this, moveY).toFloat(), duration = Constants.MainAnimationConstants.SHORT_DURATION)),
-						binding.textViewAppName.fadeIn(Constants.MainAnimationConstants.SHORT_DURATION),
-						binding.imageViewAppLogo.rotationY(360f, Constants.MainAnimationConstants.SHORT_DURATION)
-								.doOnComplete {
+                RxAnimation.sequentially(
+                        binding.imageViewAppLogo.fadeIn(Constants.MainAnimationConstants.LONG_DURATION),
+                        binding.imageViewAppLogo.resize(300, 300),
+                        binding.imageViewAppLogo.rotation(7200f, Constants.MainAnimationConstants.EXTRA_LONG_DURATION),
+                        RxAnimation.together(
+                                binding.imageViewAppLogo.resize(Constants.MainAnimationConstants.LOGO_WIDTH_INT, Constants.MainAnimationConstants.LOGO_HEIGHT_INT),
+                                binding.imageViewAppLogo.translation(pixelsToDps(this, moveX.toInt()).toFloat(), pixelsToDps(this, moveY).toFloat(), duration = Constants.MainAnimationConstants.SHORT_DURATION)),
+                        binding.textViewAppName.fadeIn(Constants.MainAnimationConstants.SHORT_DURATION),
+                        binding.imageViewAppLogo.rotationY(360f, Constants.MainAnimationConstants.SHORT_DURATION)
+                                .doOnComplete {
 
-									if (viewModel.loginSuccess.get() == true) {
-										if (viewModel.afterRegisterSuccess.get() == true) {
-											val intent = Intent(this@SplashActivity, DashboardActivity::class.java)
-											startActivity(intent)
-											finish()
-										} else {
-											val intent = Intent(this@SplashActivity, AfterRegisterActivity::class.java)
-											startActivity(intent)
-											finish()
-										}
+                                    if (viewModel.loginSuccess.get() == true) {
+                                        if (viewModel.afterRegisterSuccess.get() == true) {
+                                            val intent = Intent(this@SplashActivity, DashboardActivity::class.java)
+                                            startActivity(intent)
+                                            finish()
+                                        } else {
+                                            val intent = Intent(this@SplashActivity, AfterRegisterActivity::class.java)
+                                            startActivity(intent)
+                                            finish()
+                                        }
 
-									} else {
-										val intent = Intent(this@SplashActivity, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-										startActivity(intent)
-										finish()
-									}
+                                    } else {
+                                        val intent = Intent(this@SplashActivity, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                                        startActivity(intent)
+                                        finish()
+                                    }
 
-								}
-				).subscribe().addTo(composite)
-			}
-		}
-	}
+                                }
+                ).subscribe().addTo(composite)
+            }
+        }
+    }
 
-	override fun onDestroy() {
-		super.onDestroy()
-		composite.clear()
-	}
+    override fun onDestroy() {
+        super.onDestroy()
+        composite.clear()
+    }
 }
